64e1b635d84f3f8d8fd295fdade5c6b00370be3e
Praneeth <johndoeknown@protonmail.com>
2020-11-05 00:21:09 +0530

    added scalar data types

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,44 @@
 fn main() {
     println!("Hello World");
 
-    let immut_var:i8 = 5; // Variables are immutable by default.
-    println!("Value of immut_var is {}", immut_var);
+    let _immutable_var:i8 = 5; // Variables are immutable by default.
 
-    let mut mut_var:i8 = 2; // But mutable variables can be created using the mut keyword.
-    mut_var = mut_var + 1;
-    println!("Value of mut_var is {}", mut_var);
+    let mut _mut_var:i8 = 2; // But mutable variables can be created using the mut keyword.
+    _mut_var = _mut_var + 1;
     
-    const SOME_CONST:i8 = 3; // consts are always immutable, mut keyword doesn't work and type annotation must be given.
+    const _SOME_CONST:i8 = 3; // consts are always immutable, mut keyword doesn't work and type annotation must be given.
 
     // Shadowing
-    let spaces = "   ";
-    let spaces = spaces.len();
-    println!("Spaces = {}", spaces);
+    let _spaces = "   ";
+    let _spaces = _spaces.len();
+
+    // Scalar data types
+    // Integers
+    let _unsigned_int: u8 = 2; // Unsigned Int which takes 8 bits of space.
+    let _signed_int: i8 = -10; // Signed Int which takes 8 bits of space.
+
+    // There are sizes of 8, 16, 32, 64, 128 and arch-isize/usize which depends on the operating system's architecture.
+    // But default is 32 bit.
+
+    // Other forms of writing ints for more readability.
+    let _decimal_int = 98_222;
+    let _hex_int = 0xff;
+    let _octal_int = 0o77;
+    let _binary_int = 0b111_000;
+    let _byte_int = b'A';
+
+    // Some stuff about Integer overflow in notes
+
+    // Floating Point Numbers
+    let _float_32: f32 = 3.2; // 32 bit floating num.
+    let _float_64: f64 = 4.3; // 64 bit floating num.
+
+    // f32 is single-precision float and f64 has double precision.
+
+    // Boolean Type
+    let _login_status: bool = false;
+
+    // Character Type
+    // Declare char type using single quotes.
+    let _char_type: char = 'z';
 }
