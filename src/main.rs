549c8115e4546c0ca53b055b3b76a6828f884098
Praneeth <johndoeknown@protonmail.com>
2020-11-08 11:45:30 +0530

    added structs

--- src/main.rs
+++ src/main.rs
@@ -1,159 +1,89 @@
-// main is the root function for the program
-fn main() {
-    println!("Hello World");
+#[derive(Debug)] // Add this annotation explicitly to use Debug trait.
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool
+}
 
-    let _immutable_var:i8 = 5; // Variables are immutable by default.
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    breadth: u32
+}
 
-    let mut _mut_var:i8 = 2; // But mutable variables can be created using the mut keyword.
-    _mut_var = _mut_var + 1;
+// Methods on Rectangle struct
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.breadth
+    }
 
-    const _SOME_CONST:i8 = 3; // consts are always immutable, mut keyword doesn't work and type annotation must be given.
+    fn perimeter(&self) -> u32 {
+        2 * (self.length + self.breadth)
+    }
 
-    // Shadowing
-    let _spaces = "   ";
-    let _spaces = _spaces.len();
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length >= other.length && self.breadth >= other.breadth
+    }
+}
 
-    // Scalar data types
-    // Integers
-    let _unsigned_int: u8 = 2; // Unsigned Int which takes 8 bits of space.
-    let _signed_int: i8 = -10; // Signed Int which takes 8 bits of space.
-
-    // There are sizes of 8, 16, 32, 64, 128 and arch-isize/usize which depends on the operating system's architecture.
-    // But default is 32 bit.
-
-    // Other forms of writing ints for more readability.
-    let _decimal_int = 98_222;
-    let _hex_int = 0xff;
-    let _octal_int = 0o77;
-    let _binary_int = 0b111_000;
-    let _byte_int = b'A';
-
-    // Some stuff about Integer overflow in notes
-
-    // Floating Point Numbers
-    let _float_32: f32 = 3.2; // 32 bit floating num.
-    let _float_64: f64 = 4.3; // 64 bit floating num.
-
-    // f32 is single-precision float and f64 has double precision.
-
-    // Boolean Type
-    let _login_status: bool = false;
-
-    // Character Type
-    // Declare char type using single quotes.
-    let _char_type: char = 'z';
-
-    // Compound Types
-    // Tuple
-    let tup: (i8, u8, bool) = (-2, 3, false);
-    let (_x, _y, _z) = tup; // Pattern matching for destructuring a tuple.
-    let _tup_bool: bool = tup.2; // Accessing a tuple's element.
-
-    // Array
-    let arr: [i8; 5] = [1, 2, 3, 4, 5];
-    let _short_hand_arr: [i8; 5] = [3; 5]; // This will create an array of five 3s.
-
-    // Functions
-    greet();
-    let _five: i8 = return_int(3);
-
-    // Control Flow
-    // If statement
-    let _if_num: i32 = if false { 5 } else { 6 };
-
-    // Loop
-    let mut counter: i32 = 0;
-    let _loop_num: i32 = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter;
+// We can use multiple impl blocks on a single struct.
+impl Rectangle {
+    // associated function that doesn't take &self as param.
+    // associated functions are like new constructors to the struct to return a different instance.
+    fn make_square(size: u32) -> Rectangle {
+        Rectangle {
+            length: size,
+            breadth: size
         }
+    }
+}
+
+fn build_user(username: String, email: String) -> User {
+    // Field Init Shorthand when variables and fields have same name.
+    User {
+        username,
+        email,
+        sign_in_count: 0,
+        active: true
+    }
+}
+
+// Tuple Structs with no named fields.
+struct Color(u32, u32, u32);
+
+fn main() {
+    // Instance of the user.
+    let mut gradient = User {
+        username: String::from("Express Gradient"),
+        email: String::from("express.gradient@something.com"),
+        sign_in_count: 23,
+        active: true
     };
 
-    // While Loop
-    counter = 5;
-    while counter != 0 {
-        println!("T-{} seconds", counter);
-        counter -= 1;
-    }
-    println!("LIFTOFF!!!");
+    // To change the field, it's entire instance should be mutable.
+    gradient.email = String::from("eg@something.com");
 
-    // For Loop
-    for element in arr.iter() {
-        println!("{}", element);
-    }
+    let elliot = User {
+        username: String::from("Elliot Alderson"),
+        email: String::from("elliot.alderson@protonmail.com"),
+        ..gradient
+    };
+    println!("Elliot Struct: {:?}", elliot); // Use an struct output formatter called Debug.
 
-    // For loop with range
-    for num in (1..4).into_iter() {
-        println!("{}", num);
-    }
+    let black = Color(0, 0, 0);
+    let white = Color(255, 255, 255);
 
+    let rectangle = Rectangle {
+        length: 10,
+        breadth: 20
+    };
+    println!("Rectangle's area: {}", rectangle.area());
+    println!("Rectangle's perimeter: {}", rectangle.perimeter());
 
-    // Ownership: It is a solution to manage heap data.
-
-    // Some stuff about stack and heap in notes.
-
-    /*
-    1. Each value in Rust has a variable that's called its owner.
-    2. There can only be one owner at a time.
-    3. When the owner goes out of scope, the value will be dropped.
-    */
-
-    // Ownership scope
-    {
-        {
-            let mut _scope_var: i32 = 2; // scope_var's scope starts from here
-            _scope_var += 1; // stuff with scope_var
-        } // scope_var goes out of scope
-
-        // String Type
-        let mut some_str: String = String::from("Hello, "); // from() from String asks for memory from memory allocator at runtime.
-        some_str.push_str("World");
-        // Memory is automatically returned when the owner goes out of scope.
-
-        // Move
-        let s1: String = String::from("some string");
-        let _s2: String = s1; // s1 is no longer valid.
-
-        // Clone - Heap data gets copied, but very expensive.
-        let s1: String = String::from("some string");
-        let _s2: String = s1.clone(); // Both s1 and s2 are valid.
-
-        // Copy
-        let x: i32 = 2;
-        let _y: i32 = x; // Both x and y are valid.
-
-        // This happens because integer types have sizes known at compile time, so they are stored on stack and easy to make copies.
-        // Types which implement the Copy trait, older vars are still usable after assignment.
-    }
-
-    // References
-    {
-        let s1 = String::from("Hello, World!");
-        let len: usize = calculate_length(&s1); // Reference of s1 passed as an arg to calculate_length.
-
-        println!("{}, {}", s1, len); // s1 is perfectly valid.
-    }
-    // Some stuff about dangling pointer in notes.
-}
-
-fn greet() {
-    println!("Hello User!");
-}
-
-fn return_int(input_int: i8) -> i8 {
-    input_int // You can omit the return keyword by doing this.
-}
-
-// This process of having refs as params to a fn is called borrowing.
-fn calculate_length(s: &String) -> usize {
-    s.len()
-}
-
-// Takes in mutable reference. But you can have only one mutable reference to a piece of code in a scope.
-// This to mainly prevent data races.
-// We also cannot have mutable ref when there is an immutable ref in the scope.
-fn add_pls(input_str: &mut String) {
-    input_str.push_str(" pls");
+    let second_rectangle = Rectangle {
+        length: 5,
+        breadth: 7
+    };
+    println!("Can the rectangle hold the second_rectangle: {}", rectangle.can_hold(&second_rectangle));
 }
